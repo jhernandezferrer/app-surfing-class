7363356b90afc2cdcda20b336fa8f3083fa30c7f
Jack Stevenson <jack.stevenson@allocator.com>
2020-10-24 10:11:14 +0100

    work on bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_20_181247) do
+ActiveRecord::Schema.define(version: 2020_10_22_191424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "lesson_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["lesson_id"], name: "index_bookings_on_lesson_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "lessons", force: :cascade do |t|
     t.string "title"
@@ -41,5 +50,7 @@ ActiveRecord::Schema.define(version: 2020_10_20_181247) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "lessons"
+  add_foreign_key "bookings", "users"
   add_foreign_key "lessons", "users"
 end
